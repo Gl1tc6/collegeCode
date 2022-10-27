5fb60f482b35c466e91c6b1bfeb0a88c4beb9ad5
Gl1tc6 <93161859+Gl1tc6@users.noreply.github.com>
2022-10-27 23:24:04 +0200

    Add files via upload

--- DisMatLab/lab1.py
+++ DisMatLab/lab1.py
@@ -1,23 +1,10 @@
-def rekurzija(n, lam1, lam2, arr):
-    '''
-    :param n: željeni član rekurzivne relacije
-    :param lam1: - koef. uz n-1 član relacije
-    :param lam2: - koef. uz n-2 član relacije
-    :param arr: - osnovni članovi relacije
-    :return: n-ti član rekurzivne relacije
-    '''
-
-    if n == 0:
-        return arr[0]
-    elif n == 1:
-        return arr[1]
-    else:
-        return lam1 * rekurzija(n - 1, lam1, lam2, arr) + lam2 * rekurzija(n - 2, lam1, lam2, arr)
-
+from math import *
+import numpy as np
+from sympy import symbols, Eq, solve
 
 def lab_main():
     n = -1
-    while (n < 0):
+    while n < 0:
         n = int(input("Unesite nenegativan cijeli broj: "))
     b = []
     c = []
@@ -28,10 +15,58 @@ def lab_main():
                 b.append(int(input("Unesite vrijednost broja b_" + str(j) + ": ")))
             else:
                 c.append(int(input("Unesite vrijednost broja c_" + str(j) + ": ")))
+    # Optimizitano
+    '''
+    lam1, lam2 = symbols('lam1 lam2')
+    prva_jed = Eq(b[2] - b[1] * lam1 - b[0] * lam2, 0)
+    druga_jed = Eq(c[2] - c[1] * lam1 - c[0] * lam2, 0)
+    lam = solve((prva_jed, druga_jed), (lam1, lam2))'''
+
+    # Sporije - moj direktan kod
+    if b[1] * c[0] - b[0] * c[1] == 0:
+        print("Sustav je neodređen")
+        return 0;
 
     lam2 = (c[2] * b[1] - b[2] * c[1]) / (b[1] * c[0] - b[0] * c[1])
     lam1 = (b[2] - lam2 * b[0]) / b[1]
-    print("Vrijednost broja b_n: " + str(rekurzija(n, lam1, lam2, b)))
+
+    print(str(lam1) + "  |  " + str(lam2))
+
+
+   # lam1 = lam2 = -1
+    nul_t = np.roots([1, -1*lam1, -1*lam2])
+    print(nul_t)
+
+    A, B = symbols('A B')
+    if not isinstance(nul_t[0], complex):
+        if nul_t[0] != nul_t[1]:
+            pr_rek = Eq(b[0]-A - B, 0)
+            dr_rek = Eq(b[1] - A * nul_t[0] - B * nul_t[1], 0)
+            konst = solve((pr_rek, dr_rek), (A, B))
+            A = konst[A]
+            B = konst[B]
+            print("Vrijednost broja b_n: " + str(int(A*pow(nul_t[0], n) + B*pow(nul_t[1], n))))
+        elif nul_t[0] == nul_t[1]:
+            pr_rek = Eq(b[0]-A, 0)
+            dr_rek = Eq(b[1] - A * nul_t[0] - B * nul_t[1], 0)
+            konst = solve((pr_rek, dr_rek), (A, B))
+            A = konst[A]
+            B = konst[B]
+            print("Vrijednost broja b_n: " + str(int(A * pow(nul_t[0], n) + n * B * pow(nul_t[1], n))))
+    else:
+        x = np.real(nul_t[0])
+        y = np.imag(nul_t[0])
+        r = sqrt(x**2 + y**2)
+        fi = acos(x/r)
+        pr_rek = Eq(b[0] - A, 0)
+        dr_rek = Eq(b[1] - A * r*cos(fi) - B * r * sin(fi) , 0)
+        konst = solve((pr_rek, dr_rek), (A, B))
+        A = konst[A]
+        B = konst[B]
+        print("radius= " +str(r) + " | fi =" + str(fi)+ " | A= " + str(A) + " |B = "+ str(B))
+
+        print("Vrijednost broja b_n: " + str(int(A * pow(r, n)*cos(n*fi) + B * pow(r, n)* sin(n*fi))))
+
 
 
 lab_main()
